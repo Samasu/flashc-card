47d751f8d9c9f01bc91373e540fab1a8777ddfda
Sada <samasunari@gmail.com>
2019-10-01 23:37:25 +0900

    almost done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Card from './Card/Card';
+import DrawButton from './DrawButton/DrawButton';
 import './App.css';
 
-function App() {
+class App extends Component {
+ constructor(props) {
+   super(props);
+
+   this.updateCard = this.updateCard.bind(this);
+
+   this.state = {
+     cards: [
+       {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
+       {id: 1, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"},
+     ],
+     currentCard: {}
+   }
+ }
+
+
+  componentWillMount(){
+    const currentCards = this.state.cards;
+
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  getRandomCard(currentCards){
+     var card = currentCards[Math.floor(Math.random()* currentCards.length)];
+     return(card);
+  }
+
+ render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="cardRow">
+      <Card eng={this.state.currentCard.eng} 
+            han={this.state.currentCard.han}
+            pin={this.state.currentCard.pin}
+            />
+     </div>
+     <div className="buttonRow">
+      <DrawButton drawCard={this.updateCard}/>
+     </div>
     </div>
   );
+ }
 }
 
 export default App;
---
+++ src/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => (
+    <div className="card-container">
+     <div className="card">
+      <div className="font">
+       <div className="eng">{props.eng}</div>
+      </div>
+      <div className="back">
+       <div className="han">{props.han}</div>
+       <div className="pin">{props.pin}</div>
+      </div>
+     </div>
+    
+    </div>
+
+)
+
+export default Card
